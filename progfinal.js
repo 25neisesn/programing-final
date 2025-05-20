18a84711768896f953a83e0ae256b1db72b0a3c8
Neises <25NeisesN@badinhs.org>
2025-05-19 23:23:42 -0400

    started code for rounds of the game

--- progfinal.js
+++ progfinal.js
@@ -7,3 +7,25 @@ function generateNumber() { // generates random number
     let finalNumber = roundedDown + 1; // makes number options 1-100
     return finalNumber; // returns final number
 }
+
+function startNewGame() { // starts one round of the game
+    let randomNumber = generateNumber(); // gives new number every round
+    let guessedCorrectly = false; 
+    let guessCount = 0; //tracks guesses per round
+
+    alert("Guess the Number Game! Pick a Number 1-100"); // message to user
+    while (guessedCorrectly === false) { //starts guessing loop
+        let userInput = prompt("Enter Your Guess:"); // asks player for guess
+        if (userInput === null) { // allows player to cancel the game
+            alert("Game Cancelled");
+            return;
+        }
+        let userGuess = Number(userInput); // makes input a number
+        if (isNaN(userGuess)) { // I looked this up, NaN means not a number
+            alert("Not a number. Please Enter a Number")
+            continue; // goes to next loop
+        }
+
+    }
+
+}
